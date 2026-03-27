00c2419cd856112afee6376b41aed110106d212f
zakariya benchakouk <zakariabenchakouk34@gmail.com>
2026-03-27 18:14:41 +0100

    Disable WhiteNoise compression to fix corrupted SVG loading

--- syndic/settings/base.py
+++ syndic/settings/base.py
@@ -120,7 +120,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
